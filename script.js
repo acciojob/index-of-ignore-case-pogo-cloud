e148a672e565de39a4c35b2c3daaf0743dbd9e09
Deeptesh Biswas <67312636+pogo-cloud@users.noreply.github.com>
2024-06-27 21:20:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function indexOfIgnoreCase(s1, s2) {
-  // write your code here
+   return s1.indexOf(s2);
 }
 
 // Please do not change the code below
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
-alert(indexOfIgnoreCase(s1, s2));
+console.log(indexOfIgnoreCase(s1, s2));
